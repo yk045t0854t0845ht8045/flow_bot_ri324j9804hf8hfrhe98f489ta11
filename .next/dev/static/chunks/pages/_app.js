49ebfb9f38c0401b638dc7c365995318218330f1
MuriloFlow <255751907+MuriloFlow@users.noreply.github.com>
2026-05-27 21:07:42 -0300

    att not

---
+++ .next/dev/static/chunks/pages/_app.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/node_modules_next_dist_compiled_0o6l_m6._.js",
+  "static/chunks/node_modules_next_dist_shared_lib_0n1wg2c._.js",
+  "static/chunks/node_modules_next_dist_client_0pe1dg-._.js",
+  "static/chunks/node_modules_next_dist_0k8zr~w._.js",
+  "static/chunks/12t3_next_app_0jt-zj..js",
+  "static/chunks/[next]_entry_page-loader_ts_0j~flwh._.js",
+  "static/chunks/node_modules_react-dom_0bruynb._.js",
+  "static/chunks/node_modules_0lx093h._.js",
+  "static/chunks/[root-of-the-server]__0c0okpg._.js",
+  "static/chunks/[project]_pages__app_07xvfw~._.js",
+  "static/chunks/turbopack-[project]_pages__app_0-nzbyn._.js"
+])
